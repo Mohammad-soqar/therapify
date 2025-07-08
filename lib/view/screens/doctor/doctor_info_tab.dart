d4d18defe3c152e36a03e9fac641eb3453e5a8b8
mejo1912 <m.s.barakat2001@hotmail.com>
2025-07-08 22:48:47 +0300

    updated backbutton book appointment  quick overview

--- lib/view/screens/doctor/doctor_info_tab.dart
+++ lib/view/screens/doctor/doctor_info_tab.dart
@@ -22,18 +22,7 @@ class DoctorInfoTab extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  Icon(Ionicons.videocam, size: 18.sp, color: AppColors.primaryColor),
-                  HSpace(6.w),
-                  const Text("Instant Consultation Time"),
-                ],
-              ),
-              VSpace(8.h),
-              const Text("Mon (3 PM - 11.55 PM)"),
-              VSpace(8.h),
-              const Text("Wed (3 PM - 11.55 PM)"),
-              VSpace(30.h),
+            
               Row(
                 children: [
                   Icon(Ionicons.calendar, size: 18.sp, color: AppColors.primaryColor),
